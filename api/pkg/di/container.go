ada182d4108c21a72d22005a6122ab72e888d415
Zubairyounus99 <zubairyounus99@gmail.com>
2026-09-07 01:13:20 +0500

    Update container.go

--- api/pkg/di/container.go
+++ api/pkg/di/container.go
@@ -2051,17 +2051,9 @@ func logDriver(skipFrameCount int) *zerodriver.Logger {
 	return axiomLogger(skipFrameCount)
 }
 
-func axiomLogger(skipFrameCount int) *zerodriver.Logger {
-	axiomWriter, err := axiomzerolog.New(
-		axiomzerolog.SetLevels([]zerolog.Level{zerolog.TraceLevel, zerolog.DebugLevel, zerolog.InfoLevel, zerolog.WarnLevel, zerolog.ErrorLevel, zerolog.PanicLevel, zerolog.FatalLevel, zerolog.NoLevel}),
-		axiomzerolog.SetDataset(os.Getenv("AXIOM_DATASET_EVENTS")),
-	)
-	if err != nil {
-		log.Fatal(stacktrace.Propagatef(err, "cannot create axiom zerolog writer"))
-	}
-
-	zl := zerolog.New(axiomWriter).With().Timestamp().CallerWithSkipFrameCount(skipFrameCount).Logger()
-	return &zerodriver.Logger{Logger: &zl}
+func (c *Container) axiomLogger() (zerolog.Logger, error) {
+	output := zerolog.ConsoleWriter{Out: os.Stdout, TimeFormat: time.RFC3339}
+	return zerolog.New(output).With().Timestamp().Logger(), nil
 }
 
 func instanceID() string {
